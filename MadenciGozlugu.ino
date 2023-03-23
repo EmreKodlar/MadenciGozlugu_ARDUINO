cbc73ae783f874dcf09547cca8ca67f93216d824
EmreKodlar <76780479+EmreKodlar@users.noreply.github.com>
2023-03-23 17:22:45 +0300

    lpg değer
    lpg değer

--- MadenciGozlugu.ino
+++ MadenciGozlugu.ino
@@ -106,8 +106,8 @@ void loop() {
       delay(2000);
       }
       if(lpgDegeri>1000){        // LPG gazını oluşturan Propan gazının Alt Patlama Limiti %2.1 hacimsel
-      // üst patlama limiti ise % 9.5, Bütan gazının Alt Patlama Limiti %1.8 hacimsel, üst patlama limiti ise % 9.5 hacimseldir.
-      // Yani alt limit 1,8x500ppm= 900
+      // üst patlama limiti ise % 9.5, Bütan gazının Alt Patlama Limiti %2 hacimsel, üst patlama limiti ise % 9.5 hacimseldir.
+      // Yani alt limit 2x500ppm= 1000
       //kaynak: https://www.maden.org.tr/genel/bizden_detay.php?kod=8735
         //--------oled uyarı---
       display.clearDisplay();
